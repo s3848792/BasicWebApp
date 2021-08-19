2cc615726835c1de786fd523fdf1256a40d045fd
[William Furler s3848792] <s3848792@student.rmit.edu.au>
2021-08-19 11:29:18 +1000

    Initial commit

---
+++ src/main/java/programmers/basicwebapp/BasicWebAppApplication.java
@@ -0,0 +1,13 @@
+package programmers.basicwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicWebAppApplication.class, args);
+	}
+
+}
